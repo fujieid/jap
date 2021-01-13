7c814a3bd1dd1aa2fdb87236e1fe765daf92e73a
yadong.zhang <yadong.zhang0415@gmail.com>
2021-01-13 14:45:30 +0800

    :bulb: Add source code comments

--- jap-core/src/main/java/com/fujieid/jap/core/JapUser.java
+++ jap-core/src/main/java/com/fujieid/jap/core/JapUser.java
@@ -1,6 +1,9 @@
 package com.fujieid.jap.core;
 
 /**
+ * For the user information saved in the JAP system, the developer needs to convert the user information in the
+ * business system into JapUser.
+ *
  * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
  * @version 1.0.0
  * @date 2021/1/11 14:48
@@ -8,9 +11,26 @@ package com.fujieid.jap.core;
  */
 public class JapUser {
 
+    /**
+     * The id of the user in the developer's business system
+     */
     private String userId;
+
+    /**
+     * User name in the developer's business system
+     */
     private String username;
+
+    /**
+     * User password in developer's business system
+     */
     private String password;
+
+    /**
+     * Additional information about users in the developer's business system returned when obtaining user data.
+     * Please do not save private data, such as secret keys, token.
+     */
+    private Object additional;
 
     public String getUserId() {
         return userId;
@@ -36,6 +56,15 @@ public class JapUser {
 
     public JapUser setPassword(String password) {
         this.password = password;
+        return this;
+    }
+
+    public Object getAdditional() {
+        return additional;
+    }
+
+    public JapUser setAdditional(Object additional) {
+        this.additional = additional;
         return this;
     }
 }
