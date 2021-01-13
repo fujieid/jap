4402bb7e79a43c622df250c8db4342b3acc65687
yadong.zhang <yadong.zhang0415@gmail.com>
2021-01-13 14:48:16 +0800

    :alien: Authenticate configuration. The config parameter for each policy authenticate must be inherited from this class.

---
+++ jap-core/src/main/java/com/fujieid/jap/core/AuthenticateConfig.java
@@ -0,0 +1,12 @@
+package com.fujieid.jap.core;
+
+/**
+ * Authenticate configuration. The config parameter for each policy authenticate must be inherited from this class.
+ *
+ * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
+ * @version 1.0.0
+ * @date 2021/1/12 16:15
+ * @since 1.0.0
+ */
+public abstract class AuthenticateConfig {
+}
