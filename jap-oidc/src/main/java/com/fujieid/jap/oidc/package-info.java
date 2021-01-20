7dab6daf8201869b92497a43c402c1373a03bd19
yadong.zhang <yadong.zhang0415@gmail.com>
2021-01-20 14:18:21 +0800

    :lock: Add license for each class file

--- jap-oidc/src/main/java/com/fujieid/jap/oidc/package-info.java
+++ jap-oidc/src/main/java/com/fujieid/jap/oidc/package-info.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2020-2040, 北京符节科技有限公司 (support@fujieid.com & https://www.fujieid.com).
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * OpenID Connect 1.0 is a simple identity layer on top of the OAuth 2.0 protocol.
  * It enables Clients to verify the identity of the End-User based on the authentication performed by an Authorization Server,
