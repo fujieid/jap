1ce732e5d40ac4a3b52ef783651192204dddaa67
harrylee <harryleexyz@qq.com>
2021-01-24 16:32:37 +0800

    :new: add remember me to jap-simple

--- jap-simple/src/main/java/com/fujieid/jap/simple/SimpleStrategy.java
+++ jap-simple/src/main/java/com/fujieid/jap/simple/SimpleStrategy.java
@@ -15,17 +15,20 @@
  */
 package com.fujieid.jap.simple;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.fujieid.jap.core.AuthenticateConfig;
 import com.fujieid.jap.core.JapConfig;
 import com.fujieid.jap.core.JapUser;
 import com.fujieid.jap.core.JapUserService;
+import com.fujieid.jap.core.exception.JapException;
 import com.fujieid.jap.core.exception.JapUserException;
 import com.fujieid.jap.core.store.JapUserStore;
-import com.fujieid.jap.core.store.SessionJapUserStore;
 import com.fujieid.jap.core.strategy.AbstractJapStrategy;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * The local authentication strategy authenticates requests based on the credentials submitted through an HTML-based
@@ -70,7 +73,6 @@ public class SimpleStrategy extends AbstractJapStrategy {
         SimpleConfig simpleConfig = (SimpleConfig) config;
 
         UsernamePasswordCredential credential = this.doResolveCredential(request, simpleConfig);
-
         JapUser user = japUserService.getByName(credential.getUsername());
         if (null == user) {
             throw new JapUserException("The user does not exist.");
@@ -81,7 +83,152 @@ public class SimpleStrategy extends AbstractJapStrategy {
             throw new JapUserException("Passwords don't match.");
         }
 
+        if (simpleConfig.isEnableRememberMe() && credential.isRememberMe()) {
+            // add cookie
+            this.addRememberMeCookie(user, simpleConfig, request, response);
+        }
+
         this.loginSuccess(user, request, response);
+    }
+
+    /**
+     * math cookie
+     *
+     * @param simpleConfig config
+     * @param request      The request to authenticate
+     * @return
+     */
+    protected String matchRememberMeCookie(SimpleConfig simpleConfig, HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+
+        if ((cookies == null) || (cookies.length == 0)) {
+            return null;
+        }
+
+        for (Cookie cookie : cookies) {
+            if (ObjectUtil.equal(simpleConfig.getRememberMeCookieKey(), cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * decode UsernamePasswordCredential
+     *
+     * @param simpleConfig config
+     * @param cookieValue
+     * @return
+     */
+    protected UsernamePasswordCredential decodeCookieValue(SimpleConfig simpleConfig, String cookieValue) {
+        SimpleConfig.RememberMeDetails decode = simpleConfig.decode(cookieValue);
+        if (ObjectUtil.isNotNull(decode)) {
+            // return no longer password and remember me
+            return new UsernamePasswordCredential()
+                .setUsername(decode.getUsername());
+        }
+        return null;
+    }
+
+    /**
+     * authenticate cookie
+     *
+     * @param config   config
+     * @param request  The request to authenticate
+     * @param response The response to authenticate
+     */
+    public void authenticateCookie(AuthenticateConfig config, HttpServletRequest request, HttpServletResponse response) {
+        if (this.checkSession(request, response)) {
+            return;
+        }
+
+        // Convert AuthenticateConfig to SimpleConfig
+        this.checkAuthenticateConfig(config, SimpleConfig.class);
+        SimpleConfig simpleConfig = (SimpleConfig) config;
+        String cookieValue = this.matchRememberMeCookie(simpleConfig, request);
+
+        if (ObjectUtil.isNotNull(cookieValue)) {
+            UsernamePasswordCredential credential = this.decodeCookieValue(simpleConfig, cookieValue);
+            if (ObjectUtil.isNotNull(cookieValue)) {
+                JapUser user = japUserService.getByName(credential.getUsername());
+                if (null == user) {
+                    throw new JapUserException("The user does not exist.");
+                }
+                // redirect login successful
+                this.loginSuccess(user, request, response);
+                return;
+            }
+        }
+
+        // redirect login url
+        try {
+            response.sendRedirect(japConfig.getLoginUrl());
+        } catch (IOException e) {
+            throw new JapException("JAP failed to redirect via HttpServletResponse.", e);
+        }
+    }
+
+    /**
+     * add RememberMe Cookie
+     *
+     * @param user
+     * @param simpleConfig
+     * @param request
+     * @param response
+     */
+    protected void addRememberMeCookie(JapUser user, SimpleConfig simpleConfig, HttpServletRequest request, HttpServletResponse response) {
+        String cookieValue = this.encodeCookieValue(user, simpleConfig);
+        Cookie cookie = new Cookie(simpleConfig.getRememberMeCookieKey(), cookieValue);
+        cookie.setMaxAge(simpleConfig.getRememberMeCookieExpire());
+        cookie.setPath(this.getRequestPath(request));
+        cookie.setSecure(request.isSecure());
+        // Custom domain name
+        if (ObjectUtil.isNotEmpty(simpleConfig.getRememberMeCookieDomain())) {
+            cookie.setDomain(simpleConfig.getRememberMeCookieDomain());
+        }
+        response.addCookie(cookie);
+    }
+
+
+    /**
+     * Clear the cookie to log out of use
+     *
+     * @param simpleConfig
+     * @param request
+     * @param response
+     */
+    public void cancelRememberMeCookie(SimpleConfig simpleConfig, HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie(simpleConfig.getRememberMeCookieKey(), null);
+        cookie.setMaxAge(0);
+        cookie.setPath(this.getRequestPath(request));
+        // Custom domain name
+        if (ObjectUtil.isNotEmpty(simpleConfig.getRememberMeCookieDomain())) {
+            cookie.setDomain(simpleConfig.getRememberMeCookieDomain());
+        }
+        response.addCookie(cookie);
+    }
+
+    /**
+     * The value of the encrypted cookie
+     *
+     * @param user
+     * @param simpleConfig
+     * @return
+     */
+    protected String encodeCookieValue(JapUser user, SimpleConfig simpleConfig) {
+        return simpleConfig.encode(user.getUsername(), user.getPassword()).getEncodeValue();
+    }
+
+    /**
+     * Get the request path
+     *
+     * @param request
+     * @return
+     */
+    private String getRequestPath(HttpServletRequest request) {
+        String contextPath = request.getContextPath();
+        return contextPath.length() > 0 ? contextPath : "/";
     }
 
     private UsernamePasswordCredential doResolveCredential(HttpServletRequest request, SimpleConfig simpleConfig) {
@@ -90,8 +237,26 @@ public class SimpleStrategy extends AbstractJapStrategy {
         if (null == username || null == password) {
             throw new JapUserException("Missing credentials");
         }
+        boolean rememberMe = this.rememberRequest(request, simpleConfig);
         return new UsernamePasswordCredential()
-                .setUsername(username)
-                .setPassword(password);
+            .setUsername(username)
+            .setPassword(password)
+            .setRememberMe(rememberMe);
+    }
+
+    /**
+     * Retrieve Remember me from the request
+     *
+     * @param request
+     * @param simpleConfig
+     * @return
+     */
+    protected boolean rememberRequest(HttpServletRequest request, SimpleConfig simpleConfig) {
+        String rememberParameter = request.getParameter(simpleConfig.getRememberMeField());
+        if (rememberParameter != null) {
+            return rememberParameter.equalsIgnoreCase("true") || rememberParameter.equalsIgnoreCase("on")
+                || rememberParameter.equalsIgnoreCase("yes") || rememberParameter.equals("1");
+        }
+        return false;
     }
 }
