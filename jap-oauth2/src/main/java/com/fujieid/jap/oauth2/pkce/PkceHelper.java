8483426df6936158e0de8d744b746dc5c75ff710
yadong.zhang <yadong.zhang0415@gmail.com>
2021-01-29 21:15:34 +0800

    :ambulance: Optimize the storage mode of code_verifier in pkce and support distributed storage

--- jap-oauth2/src/main/java/com/fujieid/jap/oauth2/pkce/PkceHelper.java
+++ jap-oauth2/src/main/java/com/fujieid/jap/oauth2/pkce/PkceHelper.java
@@ -15,12 +15,13 @@
  */
 package com.fujieid.jap.oauth2.pkce;
 
-import cn.hutool.cache.CacheUtil;
-import cn.hutool.cache.impl.TimedCache;
+import com.fujieid.jap.core.cache.JapCacheContextHolder;
+import com.fujieid.jap.oauth2.OAuthConfig;
 import com.fujieid.jap.oauth2.Oauth2Util;
+import com.google.common.collect.Maps;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.Optional;
 
 /**
  * Proof Key for Code Exchange by OAuth Public Client
@@ -30,39 +31,42 @@ import java.util.concurrent.TimeUnit;
  * @see <a href="https://tools.ietf.org/html/rfc7636" target="_blank">Proof Key for Code Exchange by OAuth Public Clients</a>
  * @since 1.0.0
  */
-public class PkceUtil {
-
-    private static final TimedCache<String, String> timedCache = CacheUtil.newTimedCache(TimeUnit.MINUTES.toMillis(5));
+public class PkceHelper {
 
     /**
      * Create the parameters required by PKCE
      *
-     * @param pkceCodeChallengeMethod After the pkce enhancement protocol is enabled, the generation method of challenge
-     *                                code derived from the code verifier sent in the authorization request is `s256` by default
-     * @param params                  oauth request params
+     * @param oAuthConfig oauth config
      * @see <a href="https://tools.ietf.org/html/rfc7636#section-1.1" target="_blank">1.1. Protocol Flow</a>
      * @see <a href="https://tools.ietf.org/html/rfc7636#section-4.1" target="_blank">4.1. Client Creates a Code Verifier</a>
      * @see <a href="https://tools.ietf.org/html/rfc7636#section-4.2" target="_blank">4.2. Client Creates the Code Challenge</a>
      * @see <a href="https://tools.ietf.org/html/rfc7636#section-4.3" target="_blank"> Client Sends the Code Challenge with the Authorization Request</a>
      */
-    public static void addPkceParameters(PkceCodeChallengeMethod pkceCodeChallengeMethod, Map<String, Object> params) {
-        if (PkceCodeChallengeMethod.S256 == pkceCodeChallengeMethod) {
-            String codeVerifier = Oauth2Util.generateCodeVerifier();
-            String codeChallenge = Oauth2Util.generateCodeChallenge(pkceCodeChallengeMethod, codeVerifier);
-            params.put(PkceParams.CODE_CHALLENGE, codeChallenge);
-            params.put(PkceParams.CODE_CHALLENGE_METHOD, pkceCodeChallengeMethod);
-            // FIXME 需要考虑分布式环境，例如使用 Redis 缓存
-            timedCache.put(PkceParams.CODE_VERIFIER, codeVerifier);
+    public static Map<String, Object> generatePkceParameters(OAuthConfig oAuthConfig) {
+        /*
+        After the pkce enhancement protocol is enabled, the generation method of challenge code derived from
+        the code verifier sent in the authorization request is `s256` by default
+         */
+        PkceCodeChallengeMethod pkceCodeChallengeMethod = Optional.ofNullable(oAuthConfig.getCodeChallengeMethod())
+            .orElse(PkceCodeChallengeMethod.S256);
 
-        }
+        Map<String, Object> params = Maps.newHashMap();
+        String codeVerifier = Oauth2Util.generateCodeVerifier();
+        String codeChallenge = Oauth2Util.generateCodeChallenge(pkceCodeChallengeMethod, codeVerifier);
+        params.put(PkceParams.CODE_CHALLENGE, codeChallenge);
+        params.put(PkceParams.CODE_CHALLENGE_METHOD, pkceCodeChallengeMethod);
+        // The default cache is local map.
+        JapCacheContextHolder.getCache().set(oAuthConfig.getClientId(), codeVerifier, oAuthConfig.getCodeVerifierTimeout());
+        return params;
     }
 
     /**
      * Gets the {@code code_verifier} in the cache
      *
+     * @param clientId oauth clientId
      * @return {@code code_verifier}
      */
-    public static String getCacheCodeVerifier() {
-        return timedCache.get(PkceParams.CODE_VERIFIER);
+    public static String getCacheCodeVerifier(String clientId) {
+        return (String) JapCacheContextHolder.getCache().get(clientId);
     }
 }
