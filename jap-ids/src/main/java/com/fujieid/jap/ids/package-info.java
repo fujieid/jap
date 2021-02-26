c2f1f4cb3475ae89e5970c22b21ddc6057f9e04f
yadong.zhang <yadong.zhang0415@gmail.com>
2021-02-26 15:08:51 +0800

    :egg: jap-ids, identity-server -> oauth & oidc

---
+++ jap-ids/src/main/java/com/fujieid/jap/ids/package-info.java
@@ -0,0 +1,26 @@
+/**
+ * Authorization service based on rfc6749 protocol specification and OpenID Connect Core 1.0 specification
+ * <p>
+ * Features：<br/>
+ * 1. Authorization Code Grant <br/>
+ * 2. Implicit Grant<br/>
+ * 3. Resource Owner Password Credentials Grant<br/>
+ * 4. Refresh Token<br/>
+ * 5. Check Token<br/>
+ * 6. Proof Key for Code Exchange by OAuth Public Clients<br/>
+ * 7. OpenID Connect Discovery<br/>
+ * 8. OpenID Connect Front-Channel Logout<br/>
+ * 9. OpenID Connect Back-Channel Logout<br/>
+ * 10. ...<br/>
+ *
+ * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
+ * @version 1.0.0
+ * @see <a href="https://tools.ietf.org/html/rfc6749" target="_blank"> The OAuth 2.0 Authorization Framework</a>
+ * @see <a href="https://openid.net/specs/openid-connect-core-1_0.html" target="_blank">OpenID Connect Core 1.0 incorporating errata set 1</a>
+ * @see <a href="https://tools.ietf.org/html/rfc7636" target="_blank">Proof Key for Code Exchange by OAuth Public Clients</a>
+ * @see <a href="https://openid.net/specs/openid-connect-discovery-1_0.html" target="_blank">OpenID Connect Discovery 1.0 incorporating errata set 1</a>
+ * @see <a href="https://openid.net/specs/openid-connect-frontchannel-1_0.html" target="_blank">OpenID Connect Front-Channel Logout 1.0</a>
+ * @see <a href="https://openid.net/specs/openid-connect-backchannel-1_0.html" target="_blank">OpenID Connect Back-Channel Logout 1.0</a>
+ * @since 1.0.0
+ */
+package com.fujieid.jap.ids;
