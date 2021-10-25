8537ff25fffcf85e97d77c1c77d77d5bc581d12b
yadong.zhang <yadong.zhang0415@gmail.com>
2021-10-25 15:33:42 +0800

    :white_check_mark: test:

--- jap-ldap/src/test/java/com/fujieid/jap/ldap/template/LdapDefaultTemplateTest.java
+++ jap-ldap/src/test/java/com/fujieid/jap/ldap/template/LdapDefaultTemplateTest.java
@@ -38,32 +38,32 @@ public class LdapDefaultTemplateTest {
         LdapTemplate ldapTemplate = new LdapDefaultTemplate(dataSource);
         // 测试通过
         // clear
-        System.out.println(ldapTemplate.login("100012", "123456"));
-        // md5
         System.out.println(ldapTemplate.login("100000", "123456"));
+        // md5
+        System.out.println(ldapTemplate.login("100001", "123456"));
         // sha
         System.out.println(ldapTemplate.login("100002", "123456"));
         // ssha
-        System.out.println(ldapTemplate.login("100006", "123456"));
-        // sha512
         System.out.println(ldapTemplate.login("100003", "123456"));
-        // k5key
-        System.out.println(ldapTemplate.login("100009", "123456"));
-        // md5crypt
-        System.out.println(ldapTemplate.login("100008", "123456"));
-        // sha256crypt
+        // sha512
         System.out.println(ldapTemplate.login("100004", "123456"));
-        // sha512crypt
+        // k5key
         System.out.println(ldapTemplate.login("100005", "123456"));
+        // md5crypt
+        System.out.println(ldapTemplate.login("100006", "123456"));
+        // sha256crypt
+        System.out.println(ldapTemplate.login("100007", "123456"));
+        // sha512crypt
+        System.out.println(ldapTemplate.login("100008", "123456"));
 
 
         // 暂未测试通过
         // ext_des
-        System.out.println(ldapTemplate.login("100010", "123456"));
+        System.out.println(ldapTemplate.login("100009", "123456"));
         // smd5
-        System.out.println(ldapTemplate.login("100007", "123456"));
+        System.out.println(ldapTemplate.login("100010", "123456"));
         // crypt
-        System.out.println(ldapTemplate.login("100001", "123456"));
+        System.out.println(ldapTemplate.login("100011", "123456"));
 
     }
 }
