8443827c1f2f923c2946ece13853ae773d5325b7
yadong.zhang <yadong.zhang0415@gmail.com>
2021-11-02 17:05:32 +0800

    :bug: Fixed (Gitee #I4FGZ1)

--- jap-ids/src/main/java/com/fujieid/jap/ids/endpoint/LogoutEndpoint.java
+++ jap-ids/src/main/java/com/fujieid/jap/ids/endpoint/LogoutEndpoint.java
@@ -15,14 +15,20 @@
  */
 package com.fujieid.jap.ids.endpoint;
 
+import cn.hutool.core.util.StrUtil;
 import com.fujieid.jap.http.JapHttpRequest;
 import com.fujieid.jap.http.JapHttpResponse;
 import com.fujieid.jap.ids.JapIds;
 import com.fujieid.jap.ids.exception.IdsException;
+import com.fujieid.jap.ids.exception.InvalidTokenException;
+import com.fujieid.jap.ids.model.AccessToken;
+import com.fujieid.jap.ids.model.ClientDetail;
 import com.fujieid.jap.ids.model.IdsResponse;
 import com.fujieid.jap.ids.model.UserInfo;
+import com.fujieid.jap.ids.model.enums.ErrorResponse;
 import com.fujieid.jap.ids.pipeline.IdsPipeline;
 import com.fujieid.jap.ids.util.EndpointUtil;
+import com.fujieid.jap.ids.util.TokenUtil;
 
 /**
  * Logout Endpoint
@@ -43,7 +49,22 @@ public class LogoutEndpoint extends AbstractEndpoint {
         request.getSession().invalidate();
 
         logoutPipeline.afterHandle(request, response);
-        return new IdsResponse<String, String>()
-            .data(EndpointUtil.getLogoutRedirectUrl(request));
+
+        String accessTokenStr = TokenUtil.getAccessToken(request);
+        AccessToken accessToken = TokenUtil.getByAccessToken(accessTokenStr);
+        if (null == accessToken) {
+            throw new InvalidTokenException(ErrorResponse.INVALID_TOKEN);
+        }
+        ClientDetail clientDetail = JapIds.getContext().getClientDetailService().getByClientId(accessToken.getClientId());
+        if (null == clientDetail) {
+            throw new InvalidTokenException(ErrorResponse.INVALID_TOKEN);
+        }
+        String redirectUrl = null;
+        if (!StrUtil.isEmpty(clientDetail.getLogoutRedirectUri())) {
+            redirectUrl = clientDetail.getLogoutRedirectUri();
+        } else {
+            redirectUrl = EndpointUtil.getLogoutRedirectUrl(request);
+        }
+        return new IdsResponse<String, String>().data(redirectUrl);
     }
 }
